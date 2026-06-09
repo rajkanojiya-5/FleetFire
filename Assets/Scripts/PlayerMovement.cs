ff3419c579f0e6d4c23b73583e7ea2372a2b16c7
Abhinav <abhinavkashyap1055@gmail.com>
2026-06-09 23:33:19 +0530

    Abhinav

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 12f;
+    public float moveSpeed = 15f;
     public CharacterController controller;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
